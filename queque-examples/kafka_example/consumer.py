9714e9b9e46a6729f3a90e05945a1ea9d3310c0c
Yunus Emre Gündoğmuş <32923721+yemregundogmus@users.noreply.github.com>
2024-12-19 20:44:12 +0300

    Bugfix

--- queque-examples/kafka_example/consumer.py
+++ queque-examples/kafka_example/consumer.py
@@ -6,11 +6,6 @@ def consume():
         'mytopic',
         bootstrap_servers='kafka:9092',
         auto_offset_reset='earliest', 
-        """
-            - earliest: Tüketicinin geçmiş tüm mesajları işlemesi gerektiğinde. Örneğin, bir veri işleme uygulaması geçmiş tüm verileri analiz etmek istiyorsa bu ayar uygundur.
-            - latest: Tüketicinin yalnızca yeni mesajlarla ilgilendiği durumlarda. Örneğin, gerçek zamanlı bir işlem sisteminde, sadece yeni gelen verileri işlemek istediğinizde bu ayarı kullanabilirsiniz.
-            - none: Kesinlikle belirli bir offset'le devam edilmesi gereken ve bu offset bulunamazsa uygulamanın hata vermesi gerektiği durumlarda kullanılır.
-        """
         group_id='my-group'
     )
 
